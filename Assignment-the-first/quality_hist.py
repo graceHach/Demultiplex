e2ae22bf48a0b145d236d248fc683dd36a51284a
Grace Hach <ghach@n0351.talapas.uoregon.edu>
2024-07-30 19:46:35 -0700

    Learning all sorts of things about buffers today!

--- Assignment-the-first/quality_hist.py
+++ Assignment-the-first/quality_hist.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+print("hello, python script has begun execution",flush=True)
+
 import argparse
 import numpy as np
 import bioinfo
@@ -67,50 +69,48 @@ args = parse_args()
 
 # create array to hold all means 
 # calcualted as a running mean!
-print("starting r1 calcs")
+print("starting r1 calcs",flush=True)
 f1_means, f1_mins, f1_maxs = get_stats(args.f1_in, args.f1_length)
-print("calcs done")
+print("calcs done",flush=True)
 
 fig, ax = plt.subplots(2,2, sharey=True, figsize=(12,8),layout='constrained')
 ax[0,0].plot(range(1,102,1),f1_means,color='navy',label='Mean')
 ax[0,0].fill_between(range(1,102,1), f1_mins, f1_maxs,alpha=.2,label="Full range",color='violet')
 ax[0,0].set_title(args.f1_title)
 ax[0,0].legend(loc='lower right')
-ax[0,0].set_xlabel("Read position")
 ax[0,0].set_ylabel("Phred quality score")
 
-print("starting r2")
+print("starting r2",flush=True)
 f2_means, f2_mins, f2_maxs = get_stats(args.f2_in, args.f2_length)
-print("calcs done")
+print("calcs done",flush=True)
 
-ax[1,0].plot(range(1,102,1),f2_means,color='darkmagenta',label='Means')
-ax[1,0].fill_between(range(1,102,1), f2_mins, f2_maxs,alpha=.2,label="Full range",color='steelblue')
-ax[1,0].set_title(args.f2_title)
+ax[1,1].plot(range(1,9,1),f2_means,color='teal',label='Means')
+ax[1,1].fill_between(range(1,9,1), f2_mins, f2_maxs,alpha=.2,label="Full range",color='cadetblue')
+ax[1,1].set_title(args.f2_title)
+ax[1,1].legend(loc='lower right')
+ax[1,1].set_xlabel("Read position")
+
+print("starting r3",flush=True)
+f3_means, f3_mins, f3_maxs = get_stats(args.f3_in, args.f3_length)
+print("calcs done",flush=True)
+
+ax[0,1].plot(range(1,9,1),f3_means,color='seagreen',label='Means')
+ax[0,1].fill_between(range(1,9,1), f3_mins, f3_maxs,alpha=.2,label="Full range",color='springgreen')
+ax[0,1].set_title(args.f3_title)
+ax[0,1].legend(loc='lower right')
+
+print("starting r4",flush=True)
+f4_means, f4_mins, f4_maxs = get_stats(args.f4_in, args.f4_length)
+print("calcs done",flush=True)
+
+ax[1,0].plot(range(1,102,1),f4_means,color='maroon',label='Means')
+ax[1,0].fill_between(range(1,102,1), f4_mins, f4_maxs,alpha=.2,label="Full range",color='firebrick')
+ax[1,0].set_title(args.f4_title)
 ax[1,0].legend(loc='lower right')
 ax[1,0].set_xlabel("Read position")
 ax[1,0].set_ylabel("Phred quality score")
 
-print("starting r3")
-f3_means, f3_mins, f3_maxs = get_stats(args.f3_in, args.f3_length)
-print("calcs done")
-
-ax[0,1].plot(range(1,102,1),f3_means,color='seagreen',label='Means')
-ax[0,1].fill_between(range(1,102,1), f3_mins, f3_maxs,alpha=.2,label="Full range",color='springgreen')
-ax[0,1].set_title(args.f3_title)
-ax[0,1].legend(loc='lower right')
-ax[0,1].set_xlabel("Read position")
-ax[0,1].set_ylabel("Phred quality score")
-
-print("starting r4")
-f4_means, f4_mins, f4_maxs = get_stats(args.f4_in, args.f4_length)
-print("calcs done")
-
-ax[1,1].plot(range(1,102,1),f4_means,color='maroon',label='Means')
-ax[1,1].fill_between(range(1,102,1), f4_mins, f4_maxs,alpha=.2,label="Full range",color='firebrick')
-ax[1,1].set_title(args.f4_title)
-ax[1,1].legend(loc='lower right')
-ax[1,1].set_xlabel("Read position")
-ax[1,1].set_ylabel("Phred quality score")
+fig.suptitle("Mean, Minimum, and Maximum Quality Scores")
 
 fig.savefig(args.f_out+".png")
 
--- Assignment-the-first/run.sh
+++ Assignment-the-first/run.sh
@@ -5,11 +5,14 @@
 #SBATCH --output=LOG/quality_hists_%j.out
 #SBATCH --error=LOG/quality_hists_%j.err
 #SBATCH --job-name=quality_hists
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ghach@uoregon.edu
 
+echo "hello, run.sh is off the ground"
 set -e
 
 python quality_hist.py -f2_in "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz" \
-                       -f2_length 8 -f2_title "Read 2" \
+                        -f2_length 8 -f2_title "Read 2" \
                        -f4_in "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz" \
                        -f4_length 101 -f4_title "Read 4" \
                        -f3_in "/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz" \
